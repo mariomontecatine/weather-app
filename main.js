4119d651ed7d93be46e5bcee9655f41fd10b48d9
mario <170352659+mariomontecatine@users.noreply.github.com>
2025-12-07 17:25:31 +0100

    JS logic for showing weather done

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const apiKey = "4RXL49B3JKJWUZYUB9M9JS2BF";
 let city;
+const weatherIcons = {
+  "clear-day": "img/clear-day.png",
+  "clear-night": "img/clear-night.png",
+  cloudy: "img/cloudy.png",
+  fog: "img/fog.png",
+  "partly-cloudy-day": "img/partly-cloudy-day.png",
+  "partly-cloudy-night": "img/partly-cloudy-night.png",
+  rain: "img/rain.png",
+  snow: "img/snow.png",
+  wind: "img/wind.png",
+};
 
 function getUserInput() {
   const form = document.getElementById("cityForm");
@@ -19,14 +30,11 @@ async function weatherFetch(cityName) {
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=${apiKey}`
     );
     const data = await response.json();
-    console.log(data);
-    const dataFiltered = dataConversion(data);
-    console.log(dataFiltered);
-    let fahrenheitTemp = dataFiltered.currentConditions;
-    let celsius = toCelsius(fahrenheitTemp);
 
-    console.log(`La temperatura actual es: ${fahrenheitTemp}`);
-    console.log(`The temperature in Celsius is ${celsius.toFixed(2)}`);
+    const dataFiltered = dataConversion(data);
+    console.log("Filtrado:", dataFiltered);
+
+    displayWeather(dataFiltered); // ⬅️ AÑADIDO
   } catch (error) {
     console.error("Error", error);
   }
@@ -37,8 +45,9 @@ function dataConversion(jsonResponse) {
   let description = jsonResponse.description;
   let latitude = jsonResponse.latitude;
   let longitude = jsonResponse.longitude;
-  let alerts = jsonResponse.alerts;
+  let alerts = jsonResponse.alerts || "No alerts";
   let timezone = jsonResponse.timezone;
+  let icon = jsonResponse.currentConditions.icon;
 
   return {
     currentConditions,
@@ -47,11 +56,30 @@ function dataConversion(jsonResponse) {
     longitude,
     alerts,
     timezone,
+    icon,
   };
 }
 
 function toCelsius(fahrenheit) {
   return ((fahrenheit - 32) * 5) / 9;
+}
+
+function displayWeather(data) {
+  document.getElementById("weatherInfo").classList.remove("hidden");
+
+  document.getElementById("cityName").textContent = city;
+  document.getElementById("temp").textContent = `${toCelsius(
+    data.currentConditions
+  ).toFixed(1)} °C`;
+  document.getElementById("timezone").textContent = data.timezone;
+  document.getElementById("lat").textContent = data.latitude;
+  document.getElementById("lon").textContent = data.longitude;
+
+  document.getElementById("alerts").textContent =
+    data.alerts.length > 0 ? data.alerts[0].event : "Ninguna alerta";
+
+  const iconPath = weatherIcons[data.icon] || "img/clear-day.png";
+  document.getElementById("weatherIcon").src = iconPath;
 }
 
 getUserInput();
